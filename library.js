c1d81267d9b1e9f7c6f45197cc75aa8334ea75ee
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-12-06 00:45:14 -0800

    Add the library display and styling

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+let library = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,6 +10,44 @@ function Book(title, author, pages, read) {
     };
 }
 
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
+function addBookToLibrary(book) {
+    library.push(book);
+}
 
-console.log(book1.info());
+function displayLibrary() {
+    let container = document.querySelector("#library-container");
+    for (i in library) {
+        let bookCard = document.createElement("div");
+        bookCard.setAttribute("class", "book-card");
+        
+        let titleText = document.createElement("h2");
+        titleText.setAttribute("class", "title");
+        titleText.textContent = library[i].title;
+
+        let authorText = document.createElement("h3");
+        authorText.setAttribute("class", "author");
+        authorText.textContent = library[i].author;
+
+        let pagesText = document.createElement("p");
+        pagesText.setAttribute("class", "pages");
+        pagesText.textContent = library[i].pages + " pages";
+
+        let readText = document.createElement("p");
+        readText.setAttribute("class", "read");
+        readText.textContent = "Read? " + library[i].read;
+
+        bookCard.append(titleText, authorText, pagesText, readText);
+        container.appendChild(bookCard);
+    }
+}
+
+const book0 = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
+addBookToLibrary(book0);
+const book1 = new Book("The Way of Kings", "Brandon Sanderson", "1008", true);
+addBookToLibrary(book1);
+const book2 = new Book("Sleeping Giants", "Sylvain Neuvel", "319", false);
+addBookToLibrary(book2);
+const book3 = new Book("The Fifth Season", "N.K. Jemisin", "512", true);
+addBookToLibrary(book3);
+
+displayLibrary();

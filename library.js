f727ebb8a5fb0dc41d0ddf6753ab7552ca2f4ef9
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-12-07 14:50:42 -0800

    Consolidate newBookHandler() into addBookToLibrary()

--- library.js
+++ library.js
@@ -7,8 +7,15 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(book) {
-    library.push(book);
+function addBookToLibrary() {
+    let newTitle = document.querySelector("#new-title").value;
+    let newAuthor = document.querySelector("#new-author").value;
+    let newPages = document.querySelector("#new-pages").value;
+    let newRead = document.querySelector("#new-read").checked;
+    let newBook = new Book(newTitle, newAuthor, newPages, newRead);
+
+    library.push(newBook);
+
     displayLibrary();
 }
 
@@ -55,16 +62,6 @@ function createBookCard(book) {
     return bookCard;
 }
 
-function newBookHandler() {
-    let newTitle = document.querySelector("#new-title").value;
-    let newAuthor = document.querySelector("#new-author").value;
-    let newPages = document.querySelector("#new-pages").value;
-    let newRead = document.querySelector("#new-read").checked;
-    let newBook = new Book(newTitle, newAuthor, newPages, newRead);
-    
-    addBookToLibrary(newBook);
-}
-
 //event listeners to open, submit, and close the modal
 const addBookButton = document.querySelector("#add-book");
 const addBookModal = document.querySelector("#add-book-modal");
@@ -78,7 +75,7 @@ submitButton.addEventListener("click", submitClick);
 
 function submitClick(event) {
     event.preventDefault();
-    newBookHandler();
+    addBookToLibrary();
     form.reset();
     addBookModal.close();
 }
@@ -90,10 +87,12 @@ cancelButton.addEventListener("click", () => {
 
 //add starting books
 const book0 = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
-addBookToLibrary(book0);
+library.push(book0);
 const book1 = new Book("The Way of Kings", "Brandon Sanderson", "1008", true);
-addBookToLibrary(book1);
+library.push(book1);
 const book2 = new Book("Sleeping Giants", "Sylvain Neuvel", "319", false);
-addBookToLibrary(book2);
+library.push(book2);
 const book3 = new Book("The Fifth Season", "N.K. Jemisin", "512", true);
-addBookToLibrary(book3);
+library.push(book3);
+
+displayLibrary();
